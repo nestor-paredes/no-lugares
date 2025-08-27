3dd639c2a143cd90011007b161fd75e733c7ddc9
nestor-paredes <nestorsociologo@gmail.com>
2025-08-27 03:53:55 -0600

    indices

--- Scripts/no_lugares.R
+++ Scripts/no_lugares.R
@@ -31,17 +31,22 @@ no_lug <- no_lugares %>%
          intercam, vincula, espacio, 
          editorial, coord, contrata)
 
-rm(no_lugares) #eliminando dataframe original 
+#rm(no_lugares) #eliminando dataframe original 
 
 glimpse(no_lug) #verificando datos 
          
 #Ejecutando ACM
 
-nolugares.acm <- MCA(no_lug, excl = 1, quali.sup = 2:4, quanti.sup = 1 )
+nolugares.acm <- MCA(no_lug, quali.sup = 2:4, quanti.sup = 1 )
 
 # Resultados 
 
 nolugares.acm  
+nolugares.acm$var$contrib #contribuciones de las modalidades 
+mat_disyun <- nolugares.acm$call$Xtot #matríz disyuntiva 
+rm(mat_disyun)
+
+nolugares.acm$var$coord
 
     #Creando indices con las coordenadas de las prineras dimensiones 
     
@@ -66,13 +71,52 @@ nolugares.acm
     
     dim.1.niveles <- as.data.frame(dim.1.cat) %>% #creando dataframe y asignando id
       mutate(i_d = row_number())
+    
     dim.1.niveles
     
     no_lug_ind_cat <- no_lug_coordenadas %>%  #uniendo indice categórico
       left_join(dim.1.niveles)
     
-    rm(coor_dim.1, dim.1.cat, dim.1.niveles) #eliminando objetos de más 
+    rm(coor_dim.1, dim.1.cat, dim.1.niveles, no_lug_coordenadas) #eliminando objetos de más 
     
     
     table(no_lug_ind_cat$tipo_2, no_lug_ind_cat$dim.1.cat) #comprobando 
-#
+    #Las cátedras con niveles bajos, lo mismo para el c3, programas medios y altos, seminarios medios y altos 
+
+    #Creando índice categórico de la dim 2, usando cut simple 
+    
+    coor_dim.2 <- nolugares.acm$ind$coord[,2]  #exrayendo coordenadas dim 2
+    dim.2.cat <- cut(coor_dim.2, breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+    dim.2.niveles <- as.data.frame(dim.2.cat) %>% 
+      mutate(i_d = row_number())
+    
+    no_lug_index_cat <- no_lug_ind_cat %>% 
+      left_join(dim.2.niveles)
+    
+    no_lug_index_cat
+
+    rm(coor_dim.2, dim.2.cat, dim.2.niveles, no_lug_ind_cat)
+    
+    #índice ponderado
+    #¡¡esto es un artefacto que no tiene mucho sentido!!
+    coord.1.2 <- nolugares.acm$ind$coord[,1:2]
+    coord.1.2 
+    ind.ponderado <- nolugares.acm$ind$coord[,1:2] %*% c(.19, .14) #producto matricial coords * % inercia p/dim, 
+    ind.ponderado
+    
+    ind.ponderado.cat <- cut(ind.ponderado, #creando los niveles 
+                     breaks = quantile(ind.ponderado, probs = c(0, 1/3, 2/3, 1)), 
+                     labels = c("Bajo", "Medio", "Alto"), 
+                     include.lowest = TRUE)
+    
+    ind.ponderado.niveles <- as.data.frame(ind.ponderado.cat) %>% #creando dataframe y asignando id
+      mutate(i_d = row_number())
+    
+    no_lug_ind_gen <- no_lug_index_cat %>%  #uniendo indice categórico
+      left_join(ind.ponderado.niveles)
+    
+    nolugares.ind.ponderado.acm <- MCA(no_lug_ind_gen, quali.sup = c(2,3,4,25,26,27) , quanti.sup = c(1,23,24))
+    
+    plot(nolugares.ind.ponderado.acm , invisible=c("var"), autoLab = "yes",cex=1, habillage = "ind.ponderado.cat", col.quali.sup = "blue",
+         selectMod = c("CATEDRA", "SEMINARIO", "PROGRAMA", "CENTRO")) 
+    

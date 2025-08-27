640ef63a50e613b0726fa1351555f2d2bc3a8023
nestor-paredes <nestorsociologo@gmail.com>
2025-08-26 21:35:06 -0600

    comienzo

--- Scripts/no_lugares.R
+++ Scripts/no_lugares.R
@@ -1,1 +1,33 @@
-#hola 
+#paquetes 
+
+library(installr)
+library(readr)
+library(dplyr)
+library(tidyverse)
+library(ggthemes)
+library(GGally)
+library(corrplot)
+library(Hmisc)
+library(egg)
+library(tidyr)
+library(viridis)
+library(clipr)
+library(patchwork)
+library(FactoMineR)
+library(factoextra)
+library(Factoshiny)
+
+#Cargando datos y seleccionando variables
+
+no_lugares <- read.csv("Datos/nolugares.csv")
+
+no_lug <- no_lugares [ c("area","tipo_2", "com_dir", 
+                         "asesor_ext", "com_tec","coordinadxr", 
+                         "secret", "particip","financia",
+                         "period", "colab", "invest",
+                         "docenc", "difus", "interdis",
+                         "intercam", "vincula", "espacio", 
+                         "editorial","coord","contrata")]
+#Ejecutando ACM
+
+nolugares.acm <- MCA(no_lug, quali.sup = c("period","tipo_2","area"))

c111631039d9508c0691d9641abf9f47d7dc36ab
nestor-paredes <nestorsociologo@gmail.com>
2025-08-29 12:16:57 -0600

    mapas factoriales

--- Scripts/no_lugares.R
+++ Scripts/no_lugares.R
@@ -19,7 +19,6 @@ library(Factoshiny)
 
 #Cargando datos y seleccionando variables
 
-
 nolugares.base <- read_csv("Datos/nolugares.csv")
 
 nolugares.1 <- nolugares.base %>% 
@@ -43,92 +42,23 @@ nolugares.acm <- MCA(nolugares.1, quali.sup = c(2,3,4,22), quanti.sup = 1)
 
 # Resultados 
 nolugares.acm
-nolugares.acm$var$contrib #contribuciones de las modalidades 
-nolugares.acm$quali.sup #resultados de las suplementarias 
-
 mat_disyun <- nolugares.acm$call$Xtot #matriz disyuntiva 
 rm(mat_disyun)
 
-nolugares.acm$var$coord
+#Eigenvalues
+nolugares.acm$eig
+nolugares.acm$eig[,1] #Extrayendo % acumulados 
 
-#Creando indices con las coordenadas de las primeras dimensiones 
-    
-    coord <- nolugares.acm$ind$coord[,1:2] #extrayendo dimensiones (coordenadas)
-    
-    coord.dataframe <- as.data.frame(coord) %>%  #asignando un id
-      mutate(i_d = row_number())
-    
-    nolugares <- nolugares.1 %>% 
-      left_join(coord.dataframe, by = "i_d") #uniendo dimensiones
-    
-    rm(coord, coord.dataframe) #eliminando objetos de más 
-    
-    #Creando índice categórico de la dim 1
-    
-    coor.dim.1 <- nolugares.acm$ind$coord[,1]  #extrayendo coordenadas dim 1
-    
-    dim.1.cat <- cut(coor.dim.1, #creando los niveles 
-                      breaks = quantile(coor.dim.1, probs = c(0, 1/3, 2/3, 1)), 
-                      labels = c("Alto", "Medio", "Bajo"), 
-                      include.lowest = TRUE)
-    
-    dim.1.niveles <- as.data.frame(dim.1.cat) %>% #creando dataframe y asignando id
-      mutate(i_d = row_number())
-    
-    dim.1.niveles
-    
-    nolugares <- nolugares %>%  #uniendo indice categórico
-      left_join(dim.1.niveles, by = "i_d")
-    
-    rm(coor.dim.1,dim.1.cat,dim.1.niveles)
-    
-    #Las cátedras con niveles bajos, lo mismo para el c3, programas medios y altos, seminarios medios y altos 
-    table(nolugares$tipo_2, nolugares$dim.1.cat) #comprobando 
+write_clip(round(nolugares.acm$eig,2)) 
 
-    #Creando índice categórico de la dim 2, usando cut simple 
-    
-    coor_dim.2 <- nolugares.acm$ind$coord[,2]  #extrayendo coordenadas dim 2
-    
-    dim.2.cat <- cut(coor_dim.2, breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-    
-    dim.2.niveles <- as.data.frame(dim.2.cat) %>% 
-      mutate(i_d = row_number())
-    
-    nolugares <- nolugares %>% 
-      left_join(dim.2.niveles, by = "i_d")
-    
-    nolugares
+barplot(nolugares.acm$eig[,2], main = "Proporción de aporte de los valores propios")
 
-    rm(coor_dim.2, dim.2.cat, dim.2.niveles)
-    
-#índice ponderado
-#¡¡esto es un artefacto que no tiene mucho sentido!!
-    
-    coord.1.2 <- nolugares.acm$ind$coord[,1:2]
-    coord.1.2 
-    ind.ponderado <- nolugares.acm$ind$coord[,1:2] %*% c(.19, .14) #producto matricial coords * % inercia p/dim, se puede/debe normaliar tambien
-    ind.ponderado
-    
-    ind.ponderado.cat <- cut(ind.ponderado, #creando los niveles 
-                     breaks = quantile(ind.ponderado, probs = c(0, 1/3, 2/3, 1)), 
-                     labels = c("Bajo", "Medio", "Alto"), 
-                     include.lowest = TRUE)
-    
-    ind.ponderado.niveles <- as.data.frame(ind.ponderado.cat) %>% #creando dataframe y asignando id
-      mutate(i_d = row_number())
-    
-    nolugares <- nolugares %>%  #uniendo indice categórico
-      left_join(ind.ponderado.niveles, by = "i_d")
-    
-    nolugares.ind.ponderado.acm <- MCA(nolugares, 
-                                       quali.sup = c(2,3,4,25,26),
-                                       quanti.sup = c(1,23,24))
-    
-    plot(nolugares.ind.ponderado.acm , invisible=c("var"), 
-         autoLab = "yes",cex=1, habillage = "ind.ponderado.cat", 
-         col.quali.sup = "blue", 
-         selectMod = c("CATEDRA", "SEMINARIO", "PROGRAMA", "CENTRO"))
-    
+#Guardando gráfico 
+png("C:/Users/Néstor/Dropbox/Archivo R/Proyectos/no-lugares/Gráficos/valores_raiz.png", 
+    width = 800, height = 600)
+barplot(nolugares.acm$eig[,2], main = "Proporción de aporte de los valores propios")
+dev.off()    
+       
 #Contribuciones 
 contrib <- nolugares.acm$var$contrib[,1:3]
 round(contrib,3)
@@ -148,23 +78,10 @@ rm (contrib, coords)
 #Aporte de los individuos
 nolugares.acm$ind
 
-#Eigenvalues 
-nolugares.acm$eig
-nolugares.acm$eig[,1] #Extrayendo % acumulados 
-
-write_clip(round(no_lug_8.acm$eig,2)) 
-
-barplot(nolugares.acm$eig[,2], main = "Proporción de aporte de los valores propios")
-
-#Guardando gráfico 
-png("C:/Users/Néstor/Dropbox/Archivo R/Proyectos/no-lugares/Gráficos/valores_raiz.png", 
-    width = 800, height = 600)
-barplot(nolugares.acm$eig[,2], main = "Proporción de aporte de los valores propios")
-dev.off()
-
 #########################
 #Seleccionando dimensiones 
 
+#############
 #Dim 1 y Dim 2
 plot(nolugares.acm , invisible=c("ind"), axes=c(1,2), 
      autoLab = "yes", cex=.7,  col.var = "black", 
@@ -192,6 +109,120 @@ plot(nolugares.acm , invisible=c("ind"), axes=c(1,2),  autoLab = "yes",
                    "2011-2015","2016-2020", "2021-2024", "humanidades",
                    "ciencias")) 
 
+#Espacio factorial de los individuos dim 1 y 2
+plot(nolugares.acm, choix = c("ind"), axes=c(1,2),invisible=c("var"), 
+     col.quali.sup = "blue", autoLab = "yes",cex=.9)
+
+
+#############
+#Dim 1 y Dim 3
+plot(nolugares.acm , invisible=c("ind"), axes=c(1,3), 
+     autoLab = "yes", cex=.7,  col.var = "black", 
+     col.quali.sup = "blue", title = "A) Dimensiones 1 y 3") 
+
+#Las modalidades teóricamente relevantes 
+plot(nolugares.acm , invisible=c("ind"), axes=c(1,3),  autoLab = "yes", 
+     cex=.9,  col.var = "black", col.quali.sup = "blue", unselect = .7, 
+     selectMod = c("mixta", "mix_int", "UNAM", "1 a 5", "16 a 20","11 a 15", 
+                   "6 a 10", "cero","asesor_ext","com_dir", "coordinadxr",
+                   "com_tec", "secretaria", "intercamb", "espac", "vincula", 
+                   "contrata", "difusión", "coord", "interdisc", "edito", 
+                   "docen", "colab", "investiga", "CATEDRA", "SEMINARIO", 
+                   "PROGRAMA", "CENTRO", "1991-2005","2006-2010", 
+                   "2011-2015","2016-2020", "2021-2024", "humanidades",
+                   "ciencias")) 
+
+#Espacio factorial de los individuos dim 1 y 3
+plot(nolugares.acm, choix = c("ind"), axes=c(1,3),invisible=c("var"), 
+     col.quali.sup = "blue", autoLab = "yes",cex=.9)
+
+#############
+#Dim 2 y Dim 3
+plot(nolugares.acm , invisible=c("ind"), axes=c(2,3), 
+     autoLab = "yes", cex=.7,  col.var = "black", 
+     col.quali.sup = "blue", title = "A) Dimensiones 2 y 3") 
+
+#Las modalidades teóricamente relevantes 
+plot(nolugares.acm , invisible=c("ind"), axes=c(2,3),  autoLab = "yes", 
+     cex=.9,  col.var = "black", col.quali.sup = "blue", unselect = .7, 
+     selectMod = c("mixta", "mix_int", "UNAM", "1 a 5", "16 a 20","11 a 15", 
+                   "6 a 10", "cero","asesor_ext","com_dir", "coordinadxr",
+                   "com_tec", "secretaria", "intercamb", "espac", "vincula", 
+                   "contrata", "difusión", "coord", "interdisc", "edito", 
+                   "docen", "colab", "investiga", "CATEDRA", "SEMINARIO", 
+                   "PROGRAMA", "CENTRO", "1991-2005","2006-2010", 
+                   "2011-2015","2016-2020", "2021-2024", "humanidades",
+                   "ciencias")) 
+
+#Espacio factorial de los individuos dim 2 y 3
+plot(nolugares.acm, choix = c("ind"), axes=c(2,3),invisible=c("var"), 
+     col.quali.sup = "blue", autoLab = "yes",cex=.9)
+
+###########################
+#Clasificación 
+
+nolugares.hc <-HCPC(nolugares.acm,nb.clust=6,consol=FALSE,graph=T)
+nolugares.hc<-HCPC(nolugares.acm,nb.clust=6,consol=FALSE,graph=T, method = "ward")
+
+
+nolugares.hc$call$t$nb.clust
+no_lug_8.HCPC$call$t$nb.clust
+
+
+
+
+
+
+
+
+
+
+
+#Modalidades relevantes para la dim 1
+plot(nolugares.acm , invisible=c("ind"), axes=c(1,2), autoLab = "yes", 
+     cex=.9, col.var = "black", unselect = .7,  col.quali.sup = "blue", 
+     title = "Modalidades con mayor aporte de inercia y calidad a la Dimensión 1",
+     selectMod = c("intercamb","no_intercam", "mixta", "UNAM", "mix_int", "1 a 5", 
+                   "11 a 15", "16 a 20","espac", "no_espac","no_vincula", "vincula", 
+                   "no_colab"))
+
+#Modalidades relevantes para la dim 2
+plot(nolugares.acm , invisible=c("ind"), axes=c(1,2), autoLab = "yes", 
+     cex=.9, col.var = "black", unselect = .7,  col.quali.sup = "blue",
+     title = "a) Dimensión 2: Modalidades con mayor aporte de inercia y calidad",
+     selectMod = c("asesor_ext","no_com_tec", "secretaría","com_tec","docen",
+                   "no_secretaría", "coord", "no_interdisc", "no_docen", 
+                   "no_asesor_ex", "interdisc", "no_vincula"))
+
+
+
+
+#Combinaciones de dimensiones 1 a 3, variables suplementarias
+p1 <- plot(nolugares.acm , invisible=c("ind"), axes=c(1,2), autoLab = "yes", 
+           cex=.7, col.var = "black", unselect = .7,  col.quali.sup = "blue",
+           selectMod = c("CATEDRA", "SEMINARIO", "PROGRAMA", "CENTRo", 
+                         "1991-2005", "2006-2010", "2011-2015","2016-2020", 
+                         "2021-2024", "ciencias", "humanidades" ))
+p1
+
+p2 <- plot(nolugares.acm , invisible=c("ind"), axes=c(1,3), autoLab = "yes", 
+           cex=.7, col.var = "black", unselect = .7,  col.quali.sup = "blue",
+           selectMod = c("CATEDRA", "SEMINARIO", "PROGRAMA", "CENTRo", 
+                         "1991-2005", "2006-2010", "2011-2015","2016-2020", 
+                         "2021-2024", "ciencias", "humanidades" ))
+p2
+
+p3 <- plot(nolugares.acm , invisible=c("ind"), axes=c(2,3), autoLab = "yes", 
+           cex=.7, col.var = "black", unselect = .7,  col.quali.sup = "blue",
+           selectMod = c("CATEDRA", "SEMINARIO", "PROGRAMA", "CENTRo", 
+                         "1991-2005", "2006-2010", "2011-2015","2016-2020", 
+                         "2021-2024", "ciencias", "humanidades" ))
+p3
+
+#Agrupando los gráficos
+p1 + p2 + p3
+p1 / (p2 + p3)
+rm(p1, p2, p3)
 
 
 
@@ -205,7 +236,83 @@ plot(nolugares.acm , invisible=c("ind"), axes=c(1,2),  autoLab = "yes",
 
 
 
+#Creando indices con las coordenadas de las primeras dimensiones 
 
+coord <- nolugares.acm$ind$coord[,1:2] #extrayendo dimensiones (coordenadas)
+
+coord.dataframe <- as.data.frame(coord) %>%  #asignando un id
+  mutate(i_d = row_number())
+
+nolugares <- nolugares.1 %>% 
+  left_join(coord.dataframe, by = "i_d") #uniendo dimensiones
+
+rm(coord, coord.dataframe) #eliminando objetos de más 
+
+#Creando índice categórico de la dim 1
+
+coor.dim.1 <- nolugares.acm$ind$coord[,1]  #extrayendo coordenadas dim 1
+
+dim.1.cat <- cut(coor.dim.1, #creando los niveles 
+                 breaks = quantile(coor.dim.1, probs = c(0, 1/3, 2/3, 1)), 
+                 labels = c("Alto", "Medio", "Bajo"), 
+                 include.lowest = TRUE)
+
+dim.1.niveles <- as.data.frame(dim.1.cat) %>% #creando dataframe y asignando id
+  mutate(i_d = row_number())
+
+dim.1.niveles
+
+nolugares <- nolugares %>%  #uniendo indice categórico
+  left_join(dim.1.niveles, by = "i_d")
+
+rm(coor.dim.1,dim.1.cat,dim.1.niveles)
+
+#Las cátedras con niveles bajos, lo mismo para el c3, programas medios y altos, seminarios medios y altos 
+table(nolugares$tipo_2, nolugares$dim.1.cat) #comprobando 
+
+#Creando índice categórico de la dim 2, usando cut simple 
+
+coor_dim.2 <- nolugares.acm$ind$coord[,2]  #extrayendo coordenadas dim 2
+
+dim.2.cat <- cut(coor_dim.2, breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+
+dim.2.niveles <- as.data.frame(dim.2.cat) %>% 
+  mutate(i_d = row_number())
+
+nolugares <- nolugares %>% 
+  left_join(dim.2.niveles, by = "i_d")
+
+nolugares
+
+rm(coor_dim.2, dim.2.cat, dim.2.niveles)
+
+#índice ponderado
+#¡¡esto es un artefacto que no tiene mucho sentido!!
+
+coord.1.2 <- nolugares.acm$ind$coord[,1:2]
+coord.1.2 
+ind.ponderado <- nolugares.acm$ind$coord[,1:2] %*% c(.19, .14) #producto matricial coords * % inercia p/dim, se puede/debe normaliar tambien
+ind.ponderado
+
+ind.ponderado.cat <- cut(ind.ponderado, #creando los niveles 
+                         breaks = quantile(ind.ponderado, probs = c(0, 1/3, 2/3, 1)), 
+                         labels = c("Bajo", "Medio", "Alto"), 
+                         include.lowest = TRUE)
+
+ind.ponderado.niveles <- as.data.frame(ind.ponderado.cat) %>% #creando dataframe y asignando id
+  mutate(i_d = row_number())
+
+nolugares <- nolugares %>%  #uniendo indice categórico
+  left_join(ind.ponderado.niveles, by = "i_d")
+
+nolugares.ind.ponderado.acm <- MCA(nolugares, 
+                                   quali.sup = c(2,3,4,25,26),
+                                   quanti.sup = c(1,23,24))
+
+plot(nolugares.ind.ponderado.acm , invisible=c("var"), 
+     autoLab = "yes",cex=1, habillage = "ind.ponderado.cat", 
+     col.quali.sup = "blue", 
+     selectMod = c("CATEDRA", "SEMINARIO", "PROGRAMA", "CENTRO"))
 
 
 #######################
